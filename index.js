a4262748e33f015427bac15953420d7063e21eed
themailof <andyruizz_db@hotmail.com>
2021-04-26 18:18:32 -0500

    Creación del modelo!

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 'use strict'
 require('dotenv').config();
 var mongoose = require('mongoose');
+var app=require('./app');
+var port= process.env.PORT; 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
  }).then(()=>{
-     console.log(`.....connection to database established.....`)
+     console.log(".....connection to database established.....");
+     app.listen(port, function(){
+        console.log(".....connection to API REST server  in port http://localhost:"+port +".....");
+     });
  }).catch(err=>{
      console.log(`db error ${err.message}`);
      process.exit(-1)
