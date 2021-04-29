e0ee4d7c53fc2a85e2346b890a77f30feeab50b6
themailof <andyruizz_db@hotmail.com>
2021-04-29 05:55:03 -0500

    Registro de usuario en la BDD

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,54 @@
 'use strict'
-function pruebas(req,res){
+var bcrypt = require('bcrypt-nodejs');
+var User = require('../models/user');
+
+function pruebas(req, res) {
     res.status(200).send({
-        message:'Probando una accion del controlador de usuarios de la api'
-    }) ;
+        message: 'Probando una accion del controlador de usuarios de la api'
+    });
+}
+function saveUser(req, res) {
+    //instanciamos el modelo de usuario
+    var user = new User();
+    var params = req.body; // 
+    user.name = params.name;
+    user.surname = params.surname;
+    user.email = params.email;
+    user.role = 'ROLE_USER';
+    user.image = 'null';
+    if (params.password) {
+        // encriptar contraseña y guardar datos
+        bcrypt.hash(params.password, null, null, function (err, hash) {
+            user.password = hash;
+            if (user.name != null && user.surname != null && user.email != null) {
+                //guardar usuario
+                user.save((err, userStored) => {
+                    if (err) {
+                        res.status(500).send({ message: 'Error al guardar el usuario' });
+
+                    } else {
+                        if(!userStored){
+                            res.status(404).send({message:'No se ha registrado el usuario'});
+                        }else{
+                            res.status(200).send({user:userStored});
+                        }
+
+                    }
+                });
+
+
+            } else {
+                res.status(200).send({ message: 'Rellana todos los campos' });
+            }
+        });
+    } else {
+        res.status(500).send({ message: 'Introduce la constraseña' });
+
+    }
+
+
 }
 module.exports = {
-    pruebas
+    pruebas,
+    saveUser
 };
