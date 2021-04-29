a01d759ac8d709dd2c4c11651b8c06d5655b28b2
themailof <andyruizz_db@hotmail.com>
2021-04-29 11:00:14 -0500

    Servicio JWT

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 'use strict'
 var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
+var jwt = require('../services/jwt');
 
 function pruebas(req, res) {
     res.status(200).send({
@@ -67,6 +68,9 @@ function loginUser(req,res){
                         //devolver los datos del usuario logueado
                         if(params.getHash){
                             //devolver un token de JWT
+                            res.status(200).send({
+                                token: jwt.createToken(user)
+                            });
 
                         }else{
                             res.status(200).send({user});
